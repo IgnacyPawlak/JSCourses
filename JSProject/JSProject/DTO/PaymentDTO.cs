05d26613958198b04d8decdcf4d2feec500463ac
IgnacyPawlak <56115888+IgnacyPawlak@users.noreply.github.com>
2022-05-03 20:00:00 +0200

    dodanie paymentDTO

---
+++ JSProject/JSProject/DTO/PaymentDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JSProject.DTO
+{
+    public class PaymentDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+        public int Value { get; set; }
+    }
+}
